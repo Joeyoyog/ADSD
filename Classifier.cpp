b29523c23275e3722247df5722119b315579d4f9
Mathieu Maisonnette <mathieu.maisonnette@gmail.com>
2026-02-06 19:05:08 +0000

    4x unrolling on classify & AXI Interface
    Implemented AXI4-Master interface for input x (gmem) and AXI4-Lite for control signals.
    Added local BRAM buffer x_local to cache input image from DDR via burst read.
    Applied cyclic array partitioning (factor 4) to x_local and svs to resolve memory port contention.
    Unrolled inner distance calculation loop (classify_label1) by a factor of 4 to process 4 pixels per clock cycle.
    Pipelined inner loop with II=1 to maximize throughput.

--- Classifier.cpp
+++ Classifier.cpp
@@ -4,15 +4,37 @@
 #include "./alphas.h"
 
 double classify(ap_fixed<8,7> x[IMG_SIZE]) {
+
+	#pragma HLS INTERFACE m_axi port=x offset=slave bundle=gmem depth=784
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
+
 	ap_fixed<32,16> sum = 0.0;
+
+	//The objective is to use the 32-bits AXI interface to make it transport 4 pixels (of 8 bits) at a time
+	// Local buffer for the input image and partitioning. T
+	// This splits x_local into 4 separate RAM blocks
+	ap_fixed<8,7> x_local[IMG_SIZE];
+	#pragma HLS ARRAY_PARTITION variable=x_local cyclic factor=4 dim=1
+	//We copy the input in x_local
+	load_image_loop: for (int i = 0; i < IMG_SIZE; i++) {//785 cycles
+	        #pragma HLS PIPELINE
+	        x_local[i] = x[i];
+	    }
+
+	//We also do partition on svs
+	#pragma HLS ARRAY_PARTITION variable=svs cyclic factor=4 dim=1
 
     classify_label2:for (int i = 0; i < NSV; i++) {
         // 32 bits total, 24 bits for the integer part to prevent overflow
         ap_fixed<32,24> l2Squared_fixed = 0;
 
         classify_label1:for (int j = 0; j < IMG_SIZE; j++) {
+			#pragma HLS UNROLL factor=4// "factor=4" forces HLS to create 4 parallel logic lanes (4 roads)
+			#pragma HLS PIPELINE II=1// increases the Traffic Speed. It ensures that a new group of 4 pixels enters the lanes every clock cycle (Initiation Interval = 1)
+
             ap_fixed<8,7> xi = svs[i * IMG_SIZE + j];
-            ap_fixed<8,7> diff = xi - x[j];
+            ap_fixed<8,7> xj = x_local[j];             // Reads from x_local banks
+            ap_fixed<8,7> diff = xi - xj;
 
             // The product of two 8-bit numbers fits into 16 bits.
             // We add it to our 32-bit accumulator.
